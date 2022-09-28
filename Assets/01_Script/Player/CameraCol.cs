3da36a3601c9711fa8f9af0ac19c3c25da346435
yougong0215 <yougong0215@gmail.com>
2022-09-29 00:15:17 +0900

    09_29 수박검 위치조정

--- Assets/01_Script/Player/CameraCol.cs
+++ Assets/01_Script/Player/CameraCol.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CameraCol : MonoBehaviour
 {
-    private Transform _player;
+
     [SerializeField] CinemachineVirtualCamera _vcam;
     [SerializeField] float _originrayX;
     [SerializeField] float _originrayY;
@@ -11,7 +11,7 @@ public class CameraCol : MonoBehaviour
 
 
 
-
+    private Transform _player;
     public Transform Player
     {
         get
--- Assets/01_Script/Player/PlayerAttackManager.cs
+++ Assets/01_Script/Player/PlayerAttackManager.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class PlayerAttackManager : Singleton<PlayerAttackManager>
 {
+    [Header("Enum 들")]
     [SerializeField] PlayerStatues playerStat;
     [SerializeField] PlayerPripoty playerpri;
+
+    [Header("에니메이션")]
+    [SerializeField] Animator _ani;
+
+    private Transform _player;
+    public Transform Player
+    {
+        get
+        {
+            if (_player == null)
+            {
+                _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            }
+            return _player;
+        }
+    }
 
     public PlayerStatues PlayerS
     {
@@ -24,28 +41,37 @@ public class PlayerAttackManager : Singleton<PlayerAttackManager>
         }
     }
     Collider[] hit;
+
     void Start()
     {
         playerpri = PlayerPripoty.none;
         playerStat = PlayerStatues.Idle;
         hit = Physics.OverlapBox(transform.position, new Vector3(4, 4, 4), Quaternion.identity, 1 << (LayerMask.NameToLayer("InterectionObj")));
+        _ani = Player.GetComponent<Animator>();
     }
-    
+
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && (playerpri == PlayerPripoty.Move || playerpri == PlayerPripoty.none || playerpri == PlayerPripoty.Fight))
         {
-            playerpri = PlayerPripoty.aiming;
-            CameraManager.Instance.CiemanchineChange("ArrowView");
+            playerpri = PlayerPripoty.Fight;
+            _ani.SetTrigger("Attack");
+            //CameraManager.Instance.CiemanchineChange("ArrowView");
         }
-        if(Input.GetMouseButtonUp(0))
+        //if(Input.GetMouseButtonUp(0))
         {
-            playerpri = PlayerPripoty.none;
-            CameraManager.Instance.PlayerViewChange();
+            //playerpri = PlayerPripoty.none;
+            //CameraManager.Instance.PlayerViewChange();
         }
+    }
+
+    public void SetStateNone()
+    {
+        playerpri = PlayerPripoty.none;
+        playerStat = PlayerStatues.Idle;
     }
     
 
